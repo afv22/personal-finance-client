f1c889780821dd796a0114b048a3cdcb8e5b1445
afv22 <38698043+afv22@users.noreply.github.com>
2022-09-19 21:20:25 -0400

    Add Heroku endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 const AUTH_TOKEN = "auth-token";
 
-const GRAPHQL_ENDPOINT_URL = "http://localhost:8000/graphql/";
+const LOCAL_URL = "http://localhost:8000/graphql/";
+const HEROKU_URL = "https://afv-personal-finance.herokuapp.com/graphql/";
+
+const GRAPHQL_ENDPOINT_URL = HEROKU_URL;
 
 export { AUTH_TOKEN, GRAPHQL_ENDPOINT_URL };
